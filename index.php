ef57f0deed3db8c6826f3372bdcb7e68022dc262
Wesley-wasnietavailabledusdanmaarzo <45416129+Wesley-wasnietavailabledusdanmaarzo@users.noreply.github.com>
2021-01-15 12:04:31 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php include 'includes/header.php'; ?>
+<div class="topnav" id="myTopnav">
+    <a href="index.php" class="active">Home</a>
+    <a href="Berekenenhtml.php">Berekenen</a>
+    <a href="Boten.php">Boten</a>
+    <a href="page4.php">About</a>
+    <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+    <i class="fa fa-bars"></i>
+    </a>
+</div>
+
+<div class="flex-container">
+    <div class="Textbox1">
+        <h1>Welkom op de bestevaer website.<br></h1>
+        <h3>De Bestevaer was een nederlands vrachtschip van de rederij Phs.<br>
+        Het schip is gebouwd in 1879 door J. Laing in Sunderland.<br>
+        Het schip was tijdens de Eerste wereldoorlog gezonken door een Duitse onderzeeboot.<br>
+        Er waren 18 mensen om het leven gekomen, inclusief de kapitein.<br>
+        </h3>
+    </div>
+    <div class="Textbox2">
+        <h2>Op deze website kan u informatie vinden over Bestevaer en andere schepen.</h2>
+        <h3>
+            U kan hier ook berekenen hoe veel gewicht een ship kan hebben.<br>
+            En u kan ook informatie vinden over andere schepen.
+        </h3>
+    </div>
+</div>
+
+</div>
+</body>
+</html>
