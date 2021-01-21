9dd05b64e2c4b5d8d11179c8d19b69e8a15f476e
Wesley-wasnietavailabledusdanmaarzo <45416129+Wesley-wasnietavailabledusdanmaarzo@users.noreply.github.com>
2021-01-21 17:21:24 +0100

    Delete schepen.php

--- Bestevaer/includes/schepen.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$schepen = array(
-    "schip1" => ["Schip 1" => "Schip 1", "max_lading" => 10000, "m3" => 100],
-    "schip2" => ["Schip 2" => "Schip 2", "max_lading" => 20000, "m3" => 200],
-    "schip3" => ["Schip 3" => "Schip 3", "max_lading" => 30000, "m3" => 300],
-)
